71af44591a64ea3c17d0c74521ac43cf24d34fa1
NeroBeats <Hani.Hamshari@Hotmail.com>
2020-03-19 14:25:41 +0200

    very incompleted task

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
-
+import reducer from "./redux/reducers";
+import { Provider } from "react-redux";
 import App from "./App";
-
+import store from "./redux";
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
 registerServiceWorker();
